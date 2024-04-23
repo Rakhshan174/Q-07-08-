51570e1bae474711ce34620f1ba1e748f39a9a0a
Rakhshan174 <160410026+Rakhshan174@users.noreply.github.com>
2024-04-23 05:36:04 -0700

    Q 07 , 08 TS/JS for 45 assignment

---
+++ qes7.ts
@@ -0,0 +1,11 @@
+/* 
+Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. 
+Be sure to enclose your operation in print statements to see the result.
+qes8: You should create four lines that look like this:
+Your output should simple be foour lines with the number 8 appearing once on each line.
+*/
+
+console.log(5+3);
+console.log(10-2);
+console.log(4*2);
+console.log(16/2);
